60ccb2fbf19233600634e0ee7da1071a48b9557a
Nuutti Kotivuori <naked@iki.fi>
2022-12-21 13:09:34 +0000

    Clean up day  15

--- src/day15.rs
+++ src/day15.rs
@@ -1,6 +1,8 @@
 use test_case::test_case;
 
-fn parse_input(filename: &str) -> Result<Vec<((i64, i64), (i64, i64))>, std::io::Error> {
+type Reading = ((i64, i64), (i64, i64));
+
+fn parse_input(filename: &str) -> Result<Vec<Reading>, std::io::Error> {
     let input = std::fs::read_to_string(filename)?;
     let ret = input
         .lines()
@@ -18,14 +20,13 @@ fn parse_input(filename: &str) -> Result<Vec<((i64, i64), (i64, i64))>, std::io:
     Ok(ret)
 }
 
-fn build_spans(input: &Vec<((i64, i64), (i64, i64))>, desired_line: i64) -> Vec<(i64, bool)> {
+fn build_spans(input: &[Reading], desired_line: i64) -> Vec<(i64, bool)> {
     let mut spans: Vec<(i64, bool)> = input
         .iter()
         .flat_map(|((sx, sy), (dx, dy))| {
             let dist = (sx - dx).abs() + (sy - dy).abs();
             let ydist = (desired_line - sy).abs();
             let xdist = dist - ydist;
-            //dbg!(desired_line, *sx, *sy, *dx, *dy, dist, ydist, xdist, sx - xdist, sx + xdist);
             if xdist >= 0 {
                 vec![(sx - xdist, false), (sx + xdist, true)]
             } else {
@@ -62,11 +63,9 @@ pub fn puzzle1(filename: &str, desired_line: i64) -> Result<i64, std::io::Error>
 #[test_case("inputs/input-15", 4000000 => matches Ok(13734006908372))]
 pub fn puzzle2(filename: &str, max_coord: i64) -> Result<i64, std::io::Error> {
     let input = parse_input(filename)?;
-    //draw_map(&input);
     for y in 0..=max_coord {
         if let (Some((x, y)), _) = {
             let spans = build_spans(&input, y);
-            //dbg!(&spans);
             spans.iter().fold((None, 0), |(found, nest), (x, is_end)| {
                 if *is_end && nest == 1 && (*x + 1) >= 0 && (*x + 1) <= max_coord {
                     (Some((*x + 1, y)), 0)
@@ -79,7 +78,6 @@ pub fn puzzle2(filename: &str, max_coord: i64) -> Result<i64, std::io::Error> {
                 }
             })
         } {
-            //dbg!(x, y);
             return Ok(x * 4000000 + y);
         }
     }
